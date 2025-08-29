efc2693316e6932a754dece5fe855c90ab13155c
shohan12727 <aishohan001@gmail.com>
2025-08-29 21:00:30 +0600

    call button and copy button functionality added with function for reusuability

--- script.js
+++ script.js
@@ -17,38 +17,67 @@ for (let i = 0; i < hearts.length; i++) {
 
 }
 
-//call button functionality
-const callCoinElement = getElement('call_coin');
-let callCoin = parseInt(callCoinElement.innerText);
-
-
-document.getElementById('call_national').addEventListener('click', function () {
-  
-    if (callCoin < 20) {
-        alert('insufficient coin');
-        return;
-    }
-      callCoin -= 20;
-    alert("For National Emergency Service, Dial: 999");
-    callCoinElement.innerText = callCoin;
-    const callHistory = getElement('call_history');
-
-    const callDiv = document.createElement('div');
-    callDiv.className = 'flex items-center gap-3 bg-[#FAFAFA] p-1 mt-4';
-    callDiv.innerHTML = `
-     <div>
-       <h1 class="font-semibold">National Emergency</h1>
-       <p class="text-[#5C5C5C]">999</p>
-   </div>
-   <p class="text-[#5C5C5C]">${new Date().toLocaleTimeString()}</p>
-    `
-
-    callHistory.appendChild(callDiv);
-})
-
-
 // clear button functionality
 
 document.getElementById('clear_button').addEventListener('click', function () {
     getElement('call_history').innerHTML = ''
 })
+
+
+//copy button funtionality
+let copyCountEle = document.getElementById('copy_count');
+let copyCnt = parseInt(copyCountEle.innerText);
+
+function handleCopyButton(buttonId, number) {
+
+
+    document.getElementById(buttonId).addEventListener('click', function () {
+        const numberText = document.getElementById(number).innerText;
+        navigator.clipboard.writeText(numberText);
+        alert('Number copyed');
+        console.log('shohan');
+        copyCnt++;
+        copyCountEle.innerText = copyCnt;
+    })
+}
+handleCopyButton('nation_copy', 'national_number_id');
+handleCopyButton('police_copy', 'police_number_id');
+handleCopyButton('fire_copy', 'fire_number_id')
+
+
+// call button funtionality 
+function handleCall(buttonId, serviceName, serviceNumber) {
+    const callCoinElement = getElement('call_coin');
+
+    document.getElementById(buttonId).addEventListener('click', function () {
+        let callCoin = parseInt(callCoinElement.innerText);
+
+        if (callCoin < 20) {
+            alert('Insufficient coin');
+            return;
+        }
+
+        callCoin -= 20;
+        callCoinElement.innerText = callCoin;
+
+        alert(`For ${serviceName} Service, Dial: ${serviceNumber}`);
+
+        const callHistory = getElement('call_history');
+
+        const callDiv = document.createElement('div');
+        callDiv.className = 'flex items-center justify-between bg-[#FAFAFA] p-1 mt-4';
+        callDiv.innerHTML = `
+            <div>
+                <h1 class="font-semibold">${serviceName}</h1>
+                <p class="text-[#5C5C5C]">${serviceNumber}</p>
+            </div>
+            <p class="text-[#5C5C5C]">${new Date().toLocaleTimeString()}</p>
+             </div>
+        `;
+        callHistory.appendChild(callDiv);
+    });
+}
+handleCall('call_national', 'National Emergency', '999');
+handleCall('call_police', 'Police Service', '999');
+handleCall('fire_call', 'Fire Service', '999')
+
